8c5c0a3628327731c48f992d9329957487268768
Chris Millington <chris@themadllama.com>
2022-12-22 00:42:31 +0000

    0.05

---
+++ JS/4-ControlFlow/showLimit/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click",()=>{
+    const userInput = document.querySelector("#userInput").value;
+    const output = document.querySelector(".output");
+    output.innerHTML = " ";
+    
+    for(x=0;x<=userInput;x++){
+    // Below is original, works and is efficient, but second 
+    // solution is simpler for human to read
+
+    // x%2===0 ? output.innerHTML += `${x} is even<br>` : output.innerHTML += `${x} is odd<br>`
+    
+    const message = x%2===0 ? "Even" : "Odd";
+    output.innerHTML += `${x} - ${message} <br>`;
+}
+})
