71e47f2f3591464c3e376a238dc47c1607c29114
Chris Millington <chris@themadllama.com>
2022-12-22 00:41:55 +0000

    0.05 TruthyFalsy

---
+++ JS/4-ControlFlow/countTruthy/script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll("button");
+const input = document.querySelector("input");
+const output = document.querySelector(".output");
+let arrayDisplay = document.querySelector(".arrayDisplay");
+let arry = [];
+buttons[0].addEventListener("click",()=>{
+    let num =  input.value;
+    arry.push(num)
+    arrayDisplay.innerHTML = arry
+
+})
+buttons[1].addEventListener("click", ()=>{
+    output.innerHTML = countTruthy(arry);
+    arry=[]; 
+})
+
+function countTruthy(array){
+    let message = ""
+    let x = array.length
+    for(x=0;x<array.length;x++){
+        if(array[x]) message+=(`${array[x]} is a truthy<br>`)
+        else message+=(`${array[x]} is a falsy<br>`)
+    }
+    return(message)
+}
