c35ce7bc5f0c5a9bbbd37f921cd7f51fdbbdc949
Chris <86800596+themadllama@users.noreply.github.com>
2022-12-28 17:15:11 +0000

    0.19 SHow Primes

---
+++ JS/4-ControlFlow/showPrimes/script.js
@@ -0,0 +1,19 @@
+showPrimes(16)
+function showPrimes(num){
+    let primes =[];
+    let isItPrime = 3
+    while(isItPrime<num){
+        prime = true
+        for(let i = 2;i<isItPrime;i++)
+            if(isItPrime%i === 0){
+
+            }else{
+                
+            }
+             ? prime===false:console.log(isItPrime%i)
+        console.log(`${isItPrime} prime is ${prime}`)
+        isItPrime++
+    }
+e
+    console.log(primes)
+}
